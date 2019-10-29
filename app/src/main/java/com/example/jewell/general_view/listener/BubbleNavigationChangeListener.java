2b1c63e921ad3c5eee8d3457cd5356c2e5cb2557
Andrew <andriyKonstantinov@gmail.com>
2019-10-29 21:26:19 +0100

    Initial screen finally works.

---
+++ app/src/main/java/com/example/jewell/general_view/listener/BubbleNavigationChangeListener.java
@@ -0,0 +1,7 @@
+package com.example.jewell.general_view.listener;
+
+import android.view.View;
+
+public interface BubbleNavigationChangeListener {
+    void onNavigationChanged(View view, int position);
+}
