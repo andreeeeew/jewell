e4746b3ca63d144796fadceba54936ae511587ea
Andrew <andriyKonstantinov@gmail.com>
2019-10-29 20:07:07 +0100

    before importing new module

--- app/src/main/java/com/example/jewell/MainActivity.kt
+++ app/src/main/java/com/example/jewell/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.jewell
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        open_bottom_navigation_bar.setOnClickListener {
+            launchBottomBarActivity()
+        }
     }
+
+    private fun launchBottomBarActivity() {
+        val intent = Intent(this, BottomBarActivity::class.java)
+        startActivity(intent)
+    }
+
 }
