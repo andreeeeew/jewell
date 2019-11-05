9958527e4585ddf7645f26ebf6bcde6c6165c9a5
Andrew <andriyKonstantinov@gmail.com>
2019-11-05 19:20:18 +0100

    Something is wrong. Will come back later

--- app/src/main/java/com/example/jewell/MainActivity.kt
+++ app/src/main/java/com/example/jewell/MainActivity.kt
@@ -3,6 +3,15 @@ package com.example.jewell
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jewell.adapter.SimpleRecyclerViewAdapter
+
+
+private lateinit var recyclerView: RecyclerView
+private lateinit var recyclerViewAdapter: RecyclerView.Adapter<*>
+private lateinit var recylerViewLayoutManager: RecyclerView.LayoutManager
+private lateinit var subjects: Array<String>
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +20,43 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, BottomBarActivity::class.java)
         setContentView(R.layout.activity_main)
         startActivity(intent)
+
+        var subjects = arrayOf(
+            "Nougat",
+            "Marshmallow",
+            "Lollipop",
+            "KitKat",
+            "Jelly Bean",
+            "Ice Cream Sandwich",
+            "Gingerbread",
+            "Cupcake",
+            "dunut",
+            "eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb"
+        )
+
+        recyclerView = findViewById(R.id.recyclerview) as RecyclerView
+
+        recylerViewLayoutManager = LinearLayoutManager(this)
+        recyclerView.setLayoutManager(recylerViewLayoutManager)
+
+        recyclerViewAdapter = SimpleRecyclerViewAdapter(this@MainActivity, subjects)
+        recyclerView.setAdapter(recyclerViewAdapter)
+
+
+//        val recyclerView = findViewById(R.id.recycler_view) as RecyclerView
+//
+//// Horizontal
+//        OverScrollDecoratorHelper.setUpOverScroll(
+//            recyclerView,
+//            OverScrollDecoratorHelper.ORIENTATION_HORIZONTAL
+//        )
+//// Vertical
+//        OverScrollDecoratorHelper.setUpOverScroll(
+//            recyclerView,
+//            OverScrollDecoratorHelper.ORIENTATION_VERTICAL
+//        )
     }
 }
